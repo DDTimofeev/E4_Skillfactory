940252fcfa0f3a9ae9506d63a3d2a71980dda5d5
DDTimofeev <ddtimofeev@mail.ru>
2024-01-29 00:00:45 +0300

    Add 5 task

--- Task5.js
+++ Task5.js
@@ -1,1 +1,35 @@
-``
+class Device {
+    constructor(name, power) {
+        this.name = name;
+        this.power = power;
+        this.isTurnedOn = false;
+    }
+    getinfo()
+    {
+        console.log(this.name + " is Turned On!");
+        this.isTurnedOn = true;
+    }
+
+    getinfo()
+    {
+        return this.isTurnedOn ? this.power : 0;
+    }
+}
+
+class MadeIn extends Device {
+    constructor(fabricator, name) {
+        super(name);
+        this.fabricator = fabricator;
+    }
+
+    getinfo(){
+        if (this.name) {
+            return 'Made in ' + this.name
+        } else {
+            return super.getinfo()
+        }
+    }
+}
+
+const made = new MadeIn(true, 'China');
+console.log(made.getinfo())
